9d36989ecaed4dee5d3e28e291c16886f3e867f6
sifanfiseha <sifen.fiseha@astu.edu.et>
2025-04-14 14:31:21 +0300

    feat: add button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,7 @@
 export default function Button({ children }) {
-  return <></>;
+  return (
+    <button className="bg-Bright-Red px-6  py-3 shadow-2xl hover:bg-amber-500 duration-110 ease-in  shadow-Bright-Red rounded-full text-white uppercase font-semibold">
+      {children}
+    </button>
+  );
 }
