ffd05360c20289ddbf6654e980d703230562ad67
sifanfiseha <sifen.fiseha@astu.edu.et>
2025-04-14 14:28:41 +0300

    feat: add navbar component

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,13 +2,18 @@ import logo from "../assets/logo.svg";
 import Button from "./Button";
 
 export default function Nav() {
-  const navLink = ["pricing", "product", "about us", "careers", "comunity"];
+  const navLink = ["pricing", "product", "aboutUs", "careers", "comunity"];
   return (
-    <nav className="">
+    <nav className="flex items-center my-16 mx-32 justify-between">
       <img src={logo} alt="manage logo" />
-      <ul>
+      <ul className="flex items-center gap-8">
         {navLink.map((link) => (
-          <li key={link}>{link}</li>
+          <li
+            className="capitalize cursor-pointer hover:text-stone-700 duration-110 ease-in font-semibold"
+            key={link}
+          >
+            {link}
+          </li>
         ))}
       </ul>
       <Button>get started</Button>
